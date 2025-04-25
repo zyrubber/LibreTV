f761ac7de974192b9fa946ffcb8795c76ae2fe0e
zyrubber <72850135+zyrubber@users.noreply.github.com>
2025-04-25 14:01:50 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -38,7 +38,6 @@ const API_SITES = {
     ckzy: {
         api: 'https://www.ckzy1.com',
         name: 'CK资源',
-        adult: true
     },
     zy360: {
         api: 'https://360zy.com',
@@ -84,32 +83,26 @@ const API_SITES = {
     jkun: {
         api: 'https://jkunzyapi.com',
         name: 'jkun资源',
-        adult: true
     },
     bwzy: {
         api: 'https://api.bwzym3u8.com',
         name: '百万资源',
-        adult: true
     },
     souav: {
         api: 'https://api.souavzy.vip',
         name: 'souav资源',
-        adult: true
     },
     r155: {
         api: 'https://155api.com',
         name: '155资源',
-        adult: true
     },
     lsb: {
         api: 'https://apilsbzy1.com',
         name: 'lsb资源',
-        adult: true
     },
     huangcang: {
         api: 'https://hsckzy.vip',
         name: '黄色仓库',
-        adult: true,
         detail: 'https://hsckzy.vip'
     },
     zuid: {
@@ -119,7 +112,6 @@ const API_SITES = {
     yutu: {
         api: 'https://yutuzy10.com',
         name: '玉兔资源',
-        adult: true
     }
     // 您可以按需添加更多源
 };
